c98a7b05ae3a08bd23dc4b0172653c6157748032
Serhij S <div@altertech.com>
2023-06-21 21:08:56 +0200

    example onchange

--- eva4/include/quickstart/main.ts
+++ eva4/include/quickstart/main.ts
@@ -30,7 +30,7 @@ const show_hmi = () => {
   <div>Temperature: <span id="temp"></span></div>
   <div>Fan: <label class="switch"><input type="checkbox" id="fan"><span class="slider"></span></label></div>
 `;
-  document.getElementById("fan")!.onclick = (e) => {
+  document.getElementById("fan")!.onchange = (e) => {
     e.preventDefault();
     eva.action.toggle("unit:room1/fan").catch((err) => {
       log.error(`fan toggle error: ${err}`);
